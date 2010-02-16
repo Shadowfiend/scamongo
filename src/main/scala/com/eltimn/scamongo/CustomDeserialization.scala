16b230fa201b90b03b16016b165c10406b1965ee
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2010-02-16 16:53:17 -0500

    Added CustomSerialization with a NullSkippingSerialization trait.
    Extending this trait ensures that any fields with a setter are
    serialized, but the resulting JObject only contains the fields if the
    serialization is not null.

--- src/main/scala/com/eltimn/scamongo/CustomDeserialization.scala
+++ src/main/scala/com/eltimn/scamongo/CustomDeserialization.scala
@@ -3,35 +3,34 @@ package com.eltimn.scamongo {
 import net.liftweb.json.Formats
 import net.liftweb.json.JsonAST.JObject
 
-import scala.reflect.Manifest
 import scala.collection.mutable.HashMap
 
 import java.lang.reflect.{Constructor => JConstructor, Field, Type, Method}
 
 trait CustomDeserialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] {
-  override def create(in:JObject)(implicit formats: Formats) : BaseDocument = {
+  override def create(in: JObject)(implicit formats: Formats): BaseDocument = {
     fromJObject(in)
   }
 
-  def fromJObject(jobject:JObject) : BaseDocument
+  def fromJObject(jobject: JObject): BaseDocument
 }
 
 trait SimpleDeserialization[BaseDocument] extends CustomDeserialization[BaseDocument] {
   private val clazz = Class.forName(
     this.getClass.getName.substring(0, this.getClass.getName.length - 1))
   private val constructor = clazz.getDeclaredConstructor()
+  // Map field names to their setter methods.
   private val fieldNameMethodMap =
     new HashMap[String, Method] ++
           (for (method <- clazz.getMethods;
-               methodName = method.getName
-               if methodName.endsWith("_$eq"))
+                methodName = method.getName
+                if methodName.endsWith("_$eq"))
             yield (methodName.substring(0, methodName.length - 4) -> method))
 
-  def fromJObject(jobject:JObject) : BaseDocument = {
+  def fromJObject(jobject: JObject): BaseDocument = {
     val baseInstance = constructor.newInstance().asInstanceOf[BaseDocument]
 
     val objectMap = jobject.values
-    println(fieldNameMethodMap)
     objectMap.keys foreach { key =>
       fieldNameMethodMap.get(key) match {
         case Some(fieldSetter) => fieldSetter.invoke(baseInstance, objectMap(key).asInstanceOf[Object])
---
+++ src/main/scala/com/eltimn/scamongo/CustomSerialization.scala
@@ -0,0 +1,48 @@
+package com.eltimn.scamongo {
+
+import net.liftweb.json.Formats
+import net.liftweb.json.JsonAST._
+
+import scala.collection.mutable.HashMap
+
+import java.lang.reflect.Method
+
+trait NullSkippingSerialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] {
+  private val clazz = Class.forName(
+    this.getClass.getName.substring(0, this.getClass.getName.length - 1))
+  // Map field names to their getter methods, but only if they have a setter method.
+  private val fieldNameMethodMap =
+    new HashMap[String, Method] ++
+          (for (method <- clazz.getMethods;
+                methodName = method.getName;
+                fieldName = methodName.substring(0, methodName.length - 4)
+                if methodName.endsWith("_$eq"))
+            yield (fieldName -> clazz.getMethod(fieldName)))
+
+  override def toJObject(in: BaseDocument)(implicit formats: Formats): JObject = {
+    JObject((for ((field, method) <- fieldNameMethodMap if in != null)
+      yield JField(field, convertValueToJValue(method.invoke(in)))).toList.filter { _.value != null })
+  }
+
+  protected def convertValueToJValue(value: Any): JValue = {
+    value match {
+      case str:String   => JString(str)
+      case doub:Double  => JDouble(doub)
+      case intVal:Int   => JInt(intVal)
+      case bInt:BigInt  => JInt(bInt)
+      case bool:Boolean => JBool(bool)
+      case null         => null
+      case _ => convertUnknownToJValue(value.asInstanceOf[AnyRef])
+    }
+  }
+
+  protected def convertUnknownToJValue(value: AnyRef): JValue = {
+    val clazz = value.getClass
+    clazz.getMethods.find { method => method.getName == "toJObject" || method.getName == "toJValue" } match {
+      case Some(method) => method.invoke(value).asInstanceOf[JValue]
+      case None => null
+    }
+  }
+}
+
+}
