fd3a92fecfb9afcb77ac414c035d148a120b2478
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2010-02-23 14:36:52 -0500

    Added list support, bumped to 0.4.0.
    Only typed serialization and deserialization support lists, since lists
    are almost always of a different subtype, and therefore will require
    type information alongside them.

--- src/main/scala/com/eltimn/scamongo/CustomDeserialization.scala
+++ src/main/scala/com/eltimn/scamongo/CustomDeserialization.scala
@@ -1,11 +1,11 @@
 package com.eltimn.scamongo {
 
-  import java.lang.reflect.{Constructor => JConstructor, Field, Type, Method}
+  import java.lang.reflect.{Field, Type, Method}
 
   import scala.collection.mutable.Map
 
   import net.liftweb.json.Formats
-  import net.liftweb.json.JsonAST.JObject
+  import net.liftweb.json.JsonAST.{JObject, JArray}
   import net.liftweb.util.Log
 
   trait CustomDeserialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] {
@@ -22,6 +22,8 @@ package com.eltimn.scamongo {
      *
      * Note that this requires that the clazz object have a default
      * (no-argument) constructor.
+     *
+     * Also correctly converts JArrays to Lists before performing an assignment.
      */
     def fromJObject(jobject: JObject, clazz: Class[BaseDocument], fieldNameMethodMap: Map[String, Method]) = {
       val baseInstance = clazz.getDeclaredConstructor().newInstance().asInstanceOf[BaseDocument]
@@ -30,18 +32,35 @@ package com.eltimn.scamongo {
       objectMap.keys foreach { key =>
         fieldNameMethodMap.get(key) match {
           case Some(fieldSetter) =>
-            try {
-              fieldSetter.invoke(baseInstance, objectMap(key).asInstanceOf[Object])
-            } catch {
-              // Try to see if we tried to set the value where it needed an option
-              // for the value.
-              case _:IllegalArgumentException => fieldSetter.invoke(baseInstance, Some(objectMap(key)))
-            }
+            setFieldWithOptionFallback(baseInstance, fieldSetter, objectMap(key))
           case None => // Don't do anything with this field.
         }
       }
 
       baseInstance
+    }
+
+    /**
+     * Tries to set the field to obj using fieldSetter on instance. If setting
+     * the value obj fails, attempts to set it to Some(obj), in case the field
+     * takes Options.
+     */
+    protected def setFieldWithOptionFallback(baseInstance:BaseDocument, fieldSetter:Method, obj:Any) = {
+      try {
+        setField(baseInstance, fieldSetter, obj)
+      } catch {
+        // Try to see if we tried to set the value where it needed an option
+        // for the value.
+        case _:IllegalArgumentException => fieldSetter.invoke(baseInstance, Some(obj))
+      }
+    }
+
+    /**
+     * Tries to set the field to obj using fieldSetter on instance. Subclasses
+     * should override this method to handle special types (e.g., Lists).
+     */
+    protected def setField(instance:BaseDocument, fieldSetter:Method, obj:Any) = {
+      fieldSetter.invoke(instance, obj.asInstanceOf[Object])
     }
   }
 
@@ -72,6 +91,10 @@ package com.eltimn.scamongo {
    * Deserializes a JObject to an instance of BaseDocument while resolving any
    * types stored in scamongoType fields. These fields are set in the
    * TypingSerialization trait.
+   *
+   * Also adds support for deserialization of lists, since their items can be
+   * correctly instantiated without manifest information using the typing
+   * information embedded with the object.
    */
   trait TypeResolvingDeserialization[BaseDocument] extends SimpleDeserialization[BaseDocument] {
     override def fromJObject(jobject: JObject): BaseDocument = {
@@ -84,6 +107,19 @@ package com.eltimn.scamongo {
 
           fromJObject(jobject, clazz, fieldSetterMapForClass(clazz))
       }
+    }
+
+    override protected def setField(instance: BaseDocument, fieldSetter: Method, obj: Any) = {
+      obj match {
+        case array:JArray =>
+          fieldSetter.invoke(instance, listFromJArray(array))
+        case nonarray =>
+          fieldSetter.invoke(instance, nonarray.asInstanceOf[Object])
+      }
+    }
+
+    def listFromJArray(array:JArray) = {
+      array.values.map { value => fromJObject(value.asInstanceOf[JObject]) }
     }
 
     def classForName(className:String) = getClass.getClassLoader.loadClass(className).asInstanceOf[Class[BaseDocument]]
--- src/main/scala/com/eltimn/scamongo/CustomSerialization.scala
+++ src/main/scala/com/eltimn/scamongo/CustomSerialization.scala
@@ -1,6 +1,6 @@
 package com.eltimn.scamongo {
 
-import net.liftweb.json.Formats
+import net.liftweb.json.{Formats, DefaultFormats}
 import net.liftweb.json.JsonAST._
 
 import scala.collection.mutable.HashMap
@@ -11,7 +11,7 @@ import java.lang.reflect.Method
  * Mix in this trait if your object is serializable to a JObject.
  */
 trait JObjectSerializable {
-  def toJObject : JObject
+  def toJObject(implicit formats:Formats) : JObject
 }
 
 /**
@@ -20,7 +20,15 @@ trait JObjectSerializable {
  * object.
  */
 trait JValueSerializable {
-  def toJValue : JValue
+  def toJValue(implicit formats:Formats) : JValue
+}
+
+/**
+ * Mix in this trait to provide a default implementation of toJObject that
+ * returns the result of calling toJObject on the MongoDocument's companion.
+ */
+trait JObjectSerializableByMeta[BaseDocument] extends MongoDocument[BaseDocument] with JObjectSerializable { self: BaseDocument =>
+  override def toJObject(implicit formats:Formats) = meta.toJObject(this)
 }
 
 trait CustomSerialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] with ClassData {
@@ -29,6 +37,11 @@ trait CustomSerialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] 
       yield JField(field, convertValueToJValue(method.invoke(in)))).toList)
   }
 
+  /**
+   * Serializes primitives such as strings, numbers, and booleans to JValues.
+   * More complex objects, such as lists and other objects, are delegated to
+   * convertComplexToJValue.
+   */
   protected def convertValueToJValue(value: Any): JValue = {
     value match {
       case str:String   => JString(str)
@@ -39,11 +52,22 @@ trait CustomSerialization[BaseDocument] extends MongoDocumentMeta[BaseDocument] 
       case Some(option) => convertValueToJValue(option)
       case None         => null
       case null         => null
-      case _ => convertUnknownToJValue(value.asInstanceOf[AnyRef])
+      case _ => convertComplexToJValue(value.asInstanceOf[AnyRef])
     }
   }
 
-  protected def convertUnknownToJValue(value: AnyRef): JValue = {
+  /**
+   * Converts a complex value to a JValue. Complex values are non-primitive
+   * objects. These are serialized to null if they cannot be serialized
+   * otherwise. If they mix in the JObjectSerializable or JValueSerializable
+   * traits, they are serialized using toJObject or toJValue, respectively.
+   *
+   * Also see TypingSerialization's implementation, which adds List support,
+   * and the JObjectSerializableByMeta trait, which, when mixed into a
+   * MongoDocument, defauls the toJObject method to call the associated meta
+   * object's toJObject method for serialization.
+   */
+  protected def convertComplexToJValue(value: AnyRef): JValue = {
     value match {
       case jobjectSerializable:JObjectSerializable => jobjectSerializable.toJObject
       case jvalueSerializable:JValueSerializable   => jvalueSerializable.toJValue
@@ -69,6 +93,13 @@ trait TypingSerialization[BaseDocument <: AnyRef] extends CustomSerialization[Ba
   }
 
   def unserializableTypes: List[Class[_]] = List()
+
+  override protected def convertComplexToJValue(value: AnyRef): JValue = {
+    value match {
+      case list:List[_] => JArray(list.map { item => convertValueToJValue(item) })
+      case _ => super.convertComplexToJValue(value)
+    }
+  }
 }
 
 }
