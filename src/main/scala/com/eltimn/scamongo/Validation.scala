520427ded392ec3ddc6d41cb39a66bf9caf59132
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2010-05-21 16:09:38 -0400

    Added GlobalError subclass of DocumentError for top-level rather than field-specific errors.

--- src/main/scala/com/eltimn/scamongo/Validation.scala
+++ src/main/scala/com/eltimn/scamongo/Validation.scala
@@ -4,13 +4,17 @@ import net.liftweb.json.JsonAST.JObject
 
 import com.mongodb._
 
-trait DocumentError
+trait DocumentError {
+  def message : String
+}
 object DocumentError {
   implicit def tupleToFieldError(tuple:(String, String)) : DocumentError = tuple match {
     case (field, message) => FieldError(field, message)
   }
+  implicit def stringToGlobalError(string:String) : DocumentError = GlobalError(string)
 }
 
+case class GlobalError(message: String) extends DocumentError
 case class FieldError(field: String, message: String) extends DocumentError
 
 trait Validation {
